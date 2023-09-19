b5374bad3a7c730b2927fffa1bf0712e072ac658
root <root@Don-Walez.localdomain>
2023-09-19 03:42:51 +0100

    Update to 4-strings.c

--- 3_hexa.c
+++ 3_hexa.c
@@ -3,14 +3,14 @@
 #include <stdarg.h>
 
 /**
- * _malloc3 - allocates suitable memory space for 'binary string'
- * @num_list: list of arguments
- * @base: conversion base
- * @caps: determines whether hexadecimal is capitalised
- * Definition - determines how many bytes of memory should be
- * allocated, and performs the memory allocation.
- * Return: count of printed characters
-*/
+  * _malloc3 - allocates suitable memory space for 'binary string'
+  * @num_list: list of arguments
+  * @base: conversion base
+  * @caps: determines whether hexadecimal is capitalised
+  * Definition - determines how many bytes of memory should be
+  * allocated, and performs the memory allocation.
+  * Return: count of printed characters
+  */
 
 int _malloc3(va_list num_list, int base, char caps)
 {
@@ -46,17 +46,17 @@ int _malloc3(va_list num_list, int base, char caps)
 }
 
 /**
- * fill_buffer_hexa - fills up previously allocated memory space
- * with binary, decimal, etc. digits
- * @buffer: memory space to be filled up
- * @num: number to be converted to binary
- * @n: size of memory space
- * @base: conversion base
- * @caps: determines capitalisation of hexadecimal
- *
- * Definition - converts num to binary number and fills buffer
- * Return: count of printed characters
-*/
+  * fill_buffer_hexa - fills up previously allocated memory space
+  * with binary, decimal, etc. digits
+  * @buffer: memory space to be filled up
+  * @num: number to be converted to binary
+  * @n: size of memory space
+  * @base: conversion base
+  * @caps: determines capitalisation of hexadecimal
+
+  * Definition - converts num to binary number and fills buffer
+  * Return: count of printed characters
+  */
 
 int fill_buffer_hexa(char *buffer, int num, int n, int base, char caps)
 {
